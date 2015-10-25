aeb2a27df5785236f3a0a10767829842d6151060
suedinym <suedinym@gmail.com>
2015-10-25 08:06:49 -0400

    Added array.xml, added to colors and styles. Added base readme to project

--- app/src/main/java/com/suedinym/mememakerstarterkit/MainActivity.java
+++ app/src/main/java/com/suedinym/mememakerstarterkit/MainActivity.java
@@ -1,13 +1,10 @@
 package com.suedinym.mememakerstarterkit;
 
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
@@ -112,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.list_meme_item, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
